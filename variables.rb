c8f73d335a999e0d96028e56190f3790b3175930
Sooyung Kim <kimx1279@gmail.com>
2023-07-29 13:49:27 -0600

    Reassign variables and add puts statement

--- variables.rb
+++ variables.rb
@@ -5,3 +5,18 @@ temp_high = 90.5
 temp_low = 64.9
 sunny_today = true
 cloudy_today = true
+
+numb_of_tasks = 10
+sat = "Saturday is not full of fun and studying"
+cloudy_today = false
+temp_high = 92.9
+
+day_of_the_week = "Saturday"
+chores = "Grocery shopping"
+fun_activity = "Mountain biking"
+
+puts "Today is #{day_of_the_week}, it should be relaxing!"
+puts "Instead of doing a #{fun_activity}, I need to do #{chores}"
+puts 9 + 10
+puts day_of_the_week
+
